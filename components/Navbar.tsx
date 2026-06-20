d55e04bb336fd937344048beae3914156715e8c3
QaliAI <rubixaico@gmail.com>
2026-06-20 16:11:23 -0500

    Polish shared UI patterns and responsive styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -103,7 +103,7 @@ export function Navbar() {
                         Sign In
                       </Button>
                     </Link>
-                    <Link href="/auth/signup">
+                    <Link href="/app/create/style">
                       <Button
                         size="sm"
                         className="bg-gradient-to-r from-indigo-600 to-purple-600 hover:from-indigo-700 hover:to-purple-700 shadow-md"
